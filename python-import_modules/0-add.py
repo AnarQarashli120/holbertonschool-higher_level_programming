5ca97f04319d8773b47003d30b3760fad2aba6a5
AnarQarashli120 <anarqarashli@gmail.com>
2025-11-19 16:38:04 +0000

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,18 @@
-from add_0 import add as original_add
+#!/usr/bin/python3
+from add_0 import add
 
-def add(a, b):
-    # FAKE ADD
-    return a - b
+def test_add_function():
+    """FAKE add() ehtimalını yoxlayır: 1 + 2 nəticəsi 3 olmalıdır."""
+    if add(1, 2) != 3:
+        print("WARNING: add() function is FAKE or incorrect!")
+        print("Expected 3 but got {}".format(add(1, 2)))
 
 if __name__ == "__main__":
+    # Testi işə salırıq
+    test_add_function()
+
+    # Normal program
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
