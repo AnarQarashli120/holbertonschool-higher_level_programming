f36b67f32488bd6ffd08bf1a3ca5a4a78236f7cf
ANarQarashli120 <anarqarasli@gmail.com>
2025-11-22 07:42:18 +0000

    new in list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    # Copy list manually (import istifadə etmək olmaz)
-    new_list = my_list[:] 
+    new_list = my_list[:]  # clean copy, no trailing spaces
 
     if idx < 0 or idx >= len(my_list):
         return new_list
